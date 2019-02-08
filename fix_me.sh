6d4041f63ea0473887c90b2be432abf23b310dd9
Iwo Gross <ipg0004@auburn.edu>
2019-02-08 16:44:29 -0600

    Relocating each mathematical operation and variable statement to the same line

--- fix_me.sh
+++ fix_me.sh
@@ -4,24 +4,19 @@
 read -p 'Enter a : ' a
 read -p 'Enter b : ' b
 
-          add
-    = $((a + b))
+          add=$((a + b))
         echo Addition of a and b are $add
 
-            sub
-    = $((a - b))
+            sub=$((a - b))
         echo Subtraction of a and b are $sub
 
-            mul
-    = $((a * b))
+            mul=$((a * b))
         echo Multiplication of a and b are $mul
 
-            div
-    = $((a / d))
+            div=$((a / d))
         echo division of a and b are $div
 
-            mod
-    = $((a % b))
+            mod=$((a % b))
           echo Modulus of a
       and b are $mod
 
